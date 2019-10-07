60560f8ca51b4f65eec8b17b906d8b4e4a97b7a8
Wins Julien <julienwins060597@gmail.com>
2019-10-07 18:31:41 +0200

    mise a jour global + correction

--- isalnum.c
+++ isalnum.c
@@ -1,24 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   isalpha.c                                          :+:      :+:    :+:   */
+/*   isalnum.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jwins <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/07 09:46:52 by jwins             #+#    #+#             */
-/*   Updated: 2019/10/07 10:21:30 by jwins            ###   ########.fr       */
+/*   Created: 2019/10/07 18:02:36 by jwins             #+#    #+#             */
+/*   Updated: 2019/10/07 18:16:42 by jwins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isalpha(int argument)
+int ft_isalnum(int character)
 {
-	if ((argument >= 101 && argument <= 117) ||
-		(argument >= 120 && argument <= 127) ||
-		(argument >= 130 && argument <= 132) ||
-		(argument >= 141 && argument <= 147) ||
-		(argument >= 150 && argument <= 157) ||
-		(argument >= 160 && argument <= 167) ||
-		(argument >= 170 && argument <= 172))
-		return (1);
+	if ((character >= 48 && character <= 57) ||
+	    (character >= 65 && character <= 90) ||
+	    (character >= 97 && character <= 122))
+	    return (1);
 	return (0);
 }
--- isascii.c
+++ isascii.c
@@ -12,5 +12,7 @@
 
 int ft_isascii(int c)
 {
-
+	if (c >= 0 && c <= 127)
+		return (1);
+	return (0);
 }
