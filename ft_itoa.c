ee02924a9201db475716b3815b90474bb717d834
Julien WINS <julien.wins@std.heh.be>
2019-10-11 19:29:13 +0200

    mise a jour

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,55 +12,58 @@
 
 #include "Libft.h"
 
-int ft_digitlen(size_t nombre)
+
+int ft_digitlen(long nombre)
 {
-	size_t	size;
-	
-	if (nombre == 0)
-		return (0);
-	size = 0;
-	if (nombre < 0)
-	{
-		nombre -= nombre;
-		size++;
-	}
-	while (nombre != 0)
-	{
-		nombre /= 10;
-		size++;
-	}
-	return (size);
+    size_t    size;
+    
+    if (nombre == 0)
+        return (nombre + 1);
+    size = 0;
+    if (nombre < 0)
+    {
+        nombre = -nombre;
+        size++;
+    }
+    while (nombre != 0)
+    {
+        nombre /= 10;
+        size++;
+    }
+    return (size);
 }
 
 static int ft_sign(int n)
 {
-	if (n < 0)
-		return (1);
-	return (0);
+    if (n < 0)
+        return (1);
+    return (0);
 }
 
 char *ft_itoa(int n)
 {
-		long long_n;
-		long longueur;
-		char *fresh;
-		
-		long_n = n;
-		longueur = ft_digitlen(long_n);
-		fresh = (char *)malloc((longueur + 1) * sizeof(char));
-		if (!fresh)
-			return (NULL);
-		fresh[longueur] = '\0';
-		longueur--;
-		if (long_n < 0)
-			long_n -= long_n;
-		while (longueur >= 0)
-		{
-			fresh[longueur] = (long_n % 10) + '0';
-			longueur--;
-			long_n /= 10;
-		}
-		if (ft_sign(n))
-			fresh[0] = '-';
-		return (fresh);
+        long long_n;
+        long longueur;
+        char *fresh;
+        
+        long_n = n;
+        longueur = ft_digitlen(long_n);
+        if (longueur == 0)
+          longueur = 1;
+        fresh = (char *)malloc((longueur + 1) * sizeof(char));
+        if (!fresh)
+            return (NULL);
+        fresh[longueur] = '\0';
+        longueur--;
+        if (long_n < 0)
+            long_n = -long_n;
+        while (longueur >= 0)
+        {
+            fresh[longueur] = (long_n % 10) + '0';
+            longueur--;
+            long_n /= 10;
+        }
+        if (ft_sign(n))
+            fresh[0] = '-';
+        return (fresh);
 }
